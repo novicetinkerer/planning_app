5dc235cb00915c9136f261c87f5b4b8f475dcada
novicetinkerer <55350568+novicetinkerer@users.noreply.github.com>
2025-09-17 13:48:17 +0630

    added new variable for task

--- core/task_manager.py
+++ core/task_manager.py
@@ -3,10 +3,19 @@ from data.models import Task
 
 class TaskManager:
     def __init__(self):
-        self.tasks = []
+        self.tasks: list[Task] = []
 
     def add_task(self, task: Task):
         self.tasks.append(task)
 
     def get_tasks_for_date(self, query_date: date):
-        return [task for task in self.tasks if task.due_date == query_date]
+        return [t for t in self.tasks if t.due_date == query_date]
+
+    def get_all_tasks(self):
+        return self.tasks
+
+    def mark_done(self, task: Task):
+        task.mark_done()
+
+    def delete_task(self, task: Task):
+        self.tasks.remove(task)
--- data/models.py
+++ data/models.py
@@ -1,7 +1,31 @@
+from datetime import date
+
 class Task:
-    def __init__(self, title, due_date, priority=None, tags=None):
+    def __init__(self, title: str, due_date: date, estimate_days: int, priority: str = "Medium", done: bool = False):
         self.title = title
-        self.due_date = due_date  # datetime.date object
+        self.due_date = due_date
+        self.estimate_days = estimate_days #estimated amount of days required to complete this task
         self.priority = priority
-        self.tags = tags or []
-        self.done = False
+        self.done = done
+
+    def mark_done(self):
+        self.done = True
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "due_date": self.due_date.isoformat(),
+            "estimate_days": self.estimate_days,
+            "priority": self.priority,
+            "done": self.done
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            title=data["title"],
+            due_date=date.fromisoformat(data["due_date"]),
+            estimate_days=data["estimate_days"],
+            priority=data["priority"],
+            done=data["done"]
+        )
